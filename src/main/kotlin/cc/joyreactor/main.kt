451fcbd414679e8399323def849c93bd31016455
y2k <itwisterlx@gmail.com>
2017-04-01 14:41:37 +0300

    add telegram bot api

--- src/main/kotlin/cc/joyreactor/main.kt
+++ src/main/kotlin/cc/joyreactor/main.kt
@@ -1,9 +1,36 @@
 package cc.joyreactor
 
+import com.pengrad.telegrambot.TelegramBotAdapter
+import com.pengrad.telegrambot.UpdatesListener
+import com.pengrad.telegrambot.model.Update
+import com.pengrad.telegrambot.model.request.ParseMode
+import com.pengrad.telegrambot.request.SendMessage
+
 /**
  * Created by y2k on 31/03/2017.
  **/
 
 fun main(args: Array<String>) {
-    println("hello bot")
+    startBot(args[0]) {
+        when (it.message().text()) {
+            "posts" -> getPost()
+            else -> "<b>Список команд бота:</b>\n• <b>posts</b> - список постов из ленты"
+        }
+    }
+}
+
+private fun getPost(): String {
+    TODO()
+}
+
+private fun startBot(token: String, callback: (Update) -> String) {
+    val bot = TelegramBotAdapter.build(token)
+    bot.setUpdatesListener { updates ->
+        updates
+            .map { it to callback(it) }
+            .forEach { (update, response) ->
+                bot.execute(SendMessage(update.message().from().id(), response).parseMode(ParseMode.HTML))
+            }
+        UpdatesListener.CONFIRMED_UPDATES_ALL
+    }
 }
